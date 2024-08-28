954076a6f5a26152122e4b3f0ca4ab175192f75f
AntonioHellin <74407060+AntonioHellin@users.noreply.github.com>
2024-08-28 09:46:50 +0000

    Adding new friend part 1

--- src/App.js
+++ src/App.js
@@ -78,16 +78,38 @@ function Friend({friend}) {
 }
 
 function FormAddFriend() {
-  return <form className="form-add-friend">
-    <label>🤹‍♀️ Add friend</label>
-    <input type='text'/>
+
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if(!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name, 
+      image: `${image}?=${id}`, 
+      balance: 0, 
+    };
     
-    
-    <label>🏞️ Image URL</label>
-    <input type='text'/>
-    
-    <Button>Add</Button>
-  </form>
+    setName('');
+    setImage('https://i.pravatar.cc/48');
+  }
+
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+          <label>🤹‍♀️ Friend name</label>
+          <input type='text' value={name} onChange={e=>setName(e.target.value)}/>
+        
+          <label>🏞️ Image URL</label>
+          <input type='text' value={image} onChange={e=>setImage(e.target.value)}/>
+        
+          <Button>Add</Button>
+      </form>
+  );
 }
 
 function FormSplitBill() {
